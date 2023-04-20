8cbbb3acb43ada15990e65b6253ab5103f8bdfac
unknown <mhoneylance@gmail.com>
2023-04-21 06:17:01 +0800

    Create Hash Table Implementation.js

---
+++ Data Structures - Hashtables/Hash Table Implementation.js
@@ -0,0 +1,50 @@
+class HashTable {
+	constructor(size) {
+		this.data = new Array(size);
+	}
+
+	_hash(key) {
+		let hash = 0;
+		for (let i = 0; i < key.length; i++) {
+			hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+		}
+		return hash;
+	}
+
+	set(key, value) {
+		const address = this._hash(key);
+		if (!this.data[address]) {
+			this.data[address] = [];
+		}
+		this.data[address].push([key, value]);
+	}
+
+	get(key) {
+		const address = this._hash(key);
+		const currentBucket = this.data[address];
+		if (currentBucket) {
+			for (let i = 0; i < currentBucket.length; i++) {
+				if (currentBucket[i][0] === key) {
+					return currentBucket[i][1];
+				}
+			}
+		}
+		return undefined;
+	}
+	keys() {
+		const keysArray = [];
+		const totalItems = this.data.length
+		for (let index = 0; index < totalItems; index++) {
+			if (this.data[index]) {
+				keysArray.push(this.data[index][0][0]);
+			}
+		}
+		return keysArray;
+	}
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.set("apples", 54);
+myHashTable.set("orange", 2);
+console.log(myHashTable.keys());
