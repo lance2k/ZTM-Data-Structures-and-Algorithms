bb095de2aed7764b43521db1ec537e7d1153e7d1
unknown <mhoneylance@gmail.com>
2023-05-27 21:54:45 +0800

    Create Merge sort.js

--- Sorting/Insertion sort.js
+++ Sorting/Insertion sort.js
@@ -2,23 +2,23 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function insertionSort(array) {
 	const length = array.length;
-	for (let i = 0; i < length; i++) {
-		if (array[i] < array[0]) {
-			//move number to the first position
-			array.unshift(array.splice(i, 1)[0]);
-		} else {
-			// only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
-			if (array[i] < array[i - 1]) {
-				//find where number should go
-				for (var j = 1; j < i; j++) {
-					if (array[i] >= array[j - 1] && array[i] < array[j]) {
-						//move number to the right spot
-						array.splice(j, 0, array.splice(i, 1)[0]);
-					}
-				}
-			}
+
+	// Loop through each element in the array starting from the second element
+	for (let i = 1; i < length; i++) {
+		let current = array[i];
+		let j = i - 1;
+
+		// Keep moving the larger elements to the right until we find the correct position to insert the current element
+		while (j >= 0 && array[j] > current) {
+			array[j + 1] = array[j];
+			j--;
 		}
+
+		// Insert the current element in the first available position to its right
+		array[j + 1] = current;
 	}
 }
+
+// Call the insertionSort function and print the sorted array
 insertionSort(numbers);
 console.log(numbers);
